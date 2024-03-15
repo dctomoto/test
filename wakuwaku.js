7c7f50716569e7c469f7dae7beabb804a75d063d
dctomoto <162937471+dctomoto@users.noreply.github.com>
2024-03-15 10:06:46 +0900

    Update wakuwaku.js

--- wakuwaku.js
+++ wakuwaku.js
@@ -1,12 +1,10 @@
-<script>
-        // Function to dynamically load the external script without exposing the source URL
+
         function loadChatPluginScript() {
             var script = document.createElement('script');
-            script.src = 'https://app.aminos.ai/js/chat_plugin.js'; // The masked source URL
-            script.setAttribute('data-bot-id', '15504'); // Set any additional attributes as needed
+            script.src = 'https://app.aminos.ai/js/chat_plugin.js'; 
+            script.setAttribute('data-bot-id', '15504'); 
             document.body.appendChild(script);
         }
 
         // Load the script when the webpage is loaded
         window.onload = loadChatPluginScript;
-</script>
