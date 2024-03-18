0013faeb98cd6bb539db750b74bfb51a5e3373a2
dctomoto <162937471+dctomoto@users.noreply.github.com>
2024-03-18 15:38:34 +0900

    Delete wakuwaku.js

--- wakuwaku.js
+++
@@ -1,7 +0,0 @@
-
-        function loadChatPluginScript() {
-            var script = document.createElement('script');
-            script.src = 'https://app.aminos.ai/js/chat_plugin.js'; 
-            script.setAttribute('data-bot-id', '15504'); 
-            document.body.appendChild(script);
-        }
